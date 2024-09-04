eee06d452610b8460dd56be54bba3ca195995dcf
Juma Harun <mmbh4run@gmail.com>
2024-09-04 21:27:22 +0300

    Set up express server

---
+++ backend/src/index.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import { config } from "dotenv";
+
+config();
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
