924b041e193c4b96c29dc87c8b4df0756f122e73
Juma Harun <mmbh4run@gmail.com>
2024-09-05 16:34:19 +0300

    skipped app test

--- backend/src/__tests__/app.test.ts
+++ backend/src/__tests__/app.test.ts
@@ -2,7 +2,7 @@ import request from "supertest";
 import { closeDatabaseConnection } from "../config/database.ts";
 import { createTestApp } from "../utils/testHelpers.ts";
 
-describe("GET /", () => {
+describe.skip("GET /", () => {
   afterAll(async () => {
     await closeDatabaseConnection();
   });
