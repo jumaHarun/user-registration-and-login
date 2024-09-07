d48dbb4adde5966e9136f4ffb280e233b4328f71
Juma Harun <mmbh4run@gmail.com>
2024-09-07 19:27:13 +0300

    Added auth route to file

--- backend/src/utils/testHelpers.ts
+++ backend/src/utils/testHelpers.ts
@@ -1,10 +1,25 @@
 import express from "express";
 import { connectToDB } from "../config/database.ts";
+import authRoutes from "../routes/authRoutes.ts";
+import { errorHandler } from "../middlewares/errorHandler.ts";
 
 export const createTestApp = async () => {
   const app = express();
+  app.use(express.json());
 
-  await connectToDB();
+  main();
+  async function main() {
+    try {
+      await connectToDB();
+    } catch (error) {
+      console.error(error);
+      process.exit(1);
+    } finally {
+    }
+  }
+
+  app.use("/api/auth", authRoutes);
+  app.use(errorHandler);
 
   app.get("/", (req, res) => {
     res.send("Hello, World!");
