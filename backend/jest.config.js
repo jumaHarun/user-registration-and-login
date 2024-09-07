5a33ae1106411342919d5c4ff700bb389c04f96d
Juma Harun <mmbh4run@gmail.com>
2024-09-07 16:50:42 +0300

    ignore mock_user data in gitignore. Added new jest config

---
+++ backend/jest.config.js
@@ -0,0 +1,16 @@
+/** @type {import("ts-jest").JestConfigWithTsJest} */
+const config = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  roots: ["./src/__tests__"],
+  transform: {
+    "^.+\\.tsx?$": "babel-jest",
+  },
+  testRegex: "(__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
+};
+
+export default config;
