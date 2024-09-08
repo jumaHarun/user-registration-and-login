3632b4dee2ae79d1ca3caa2918368a8283b0845a
Juma Harun <mmbh4run@gmail.com>
2024-09-08 18:56:54 +0300

    Set up JWT and used helper functions

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -1,8 +1,16 @@
 import { Request, Response } from "express";
 import asyncHandler from "express-async-handler";
-import bcrypt from "bcrypt";
-import { getDB } from "../config/database.ts";
-import { createUser } from "../repos/userRepo.ts";
+import { config } from "dotenv";
+import jwt from "jsonwebtoken";
+import { createUser, findUserByEmail } from "../repos/userRepo.ts";
+import { hashPassword, isUserPassword } from "../utils/databaseHelpers.ts";
+
+config({ path: "../.env" });
+
+const jwtSecret = process.env.JWT_SECRET_KEY;
+if (!jwtSecret) {
+  throw new Error("Please set your JWT_SECRET_KEY in the .env file.");
+}
 
 const saltOrRounds = 10;
 
@@ -18,20 +26,53 @@ export const registerUser = asyncHandler(
       return;
     }
 
-    const db = await getDB();
-    const existingUser = await db.collection("users").findOne({ email });
+    const existingUser = await findUserByEmail(email);
     if (existingUser) {
       res.status(400).json({ message: "Email already registered" });
       return;
     }
 
-    const hashedPassword = await bcrypt.hash(password, saltOrRounds);
+    const hashedPassword = await hashPassword(password, saltOrRounds);
 
     const newUser = await createUser(email, hashedPassword);
+
+    const token = jwt.sign({ userId: newUser._id }, jwtSecret, {
+      expiresIn: "1h",
+    });
 
     res.status(201).json({
       message: `User registered successfully`,
       user: newUser,
+      token,
     });
   }
 );
+
+export const loginUser = asyncHandler(async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+  if (!email) {
+    res.status(400).json({ message: "Please provide an email address" });
+    return;
+  } else if (!password) {
+    res.status(400).json({ message: "Please provide a password" });
+    return;
+  }
+
+  const user = await findUserByEmail(email);
+  if (!user) {
+    res.status(400).json({ message: "Invalid email" });
+    return;
+  }
+
+  const isMatch = await isUserPassword(password, user.password);
+  if (!isMatch) {
+    res.status(401).json({ message: "Invalid password" });
+    return;
+  }
+
+  const token = jwt.sign({ userId: user._id }, jwtSecret, {
+    expiresIn: "1h",
+  });
+
+  res.status(200).json({ message: "Login successful", token });
+});
