9d08b26d600c68ad51aebd5a9d18d355733a232a
Juma Harun <mmbh4run@gmail.com>
2024-09-19 00:19:19 +0300

    Implemented handlers for button clicks

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -10,22 +10,42 @@ interface HomeProps {
 const Home = ({ loggedIn, email, setLoggedIn }: HomeProps) => {
   const navigate = useNavigate();
 
-  const onClickHandler = () => {
-    // TDB
+  const loginHandler = () => {
+    if (loggedIn) {
+      setLoggedIn(false);
+    } else {
+      navigate("/login");
+    }
+    return;
+  };
+
+  const signUpHandler = () => {
+    navigate("/register");
   };
 
   return (
     <div className="mainContainer">
       <div className="titleContainer">
-        <div>Title here</div>
+        <div>Welcome!</div>
       </div>
       <div>This is the Homepage</div>
       <div className="buttonContainer">
-        <input
-          type="button"
-          className="inputButton"
-          value={loggedIn ? "Log out" : "Log in"}
-        />
+        <div className="inputContainer">
+          <button
+            className="inputBox"
+            onClick={loginHandler}
+          >
+            {loggedIn ? "Log out" : "Log in"}
+          </button>
+          {!loggedIn && (
+            <button
+              className="inputBox"
+              onClick={signUpHandler}
+            >
+              Sign up
+            </button>
+          )}
+        </div>
         <div>{loggedIn && `Your email address is ${email}`}</div>
       </div>
     </div>
