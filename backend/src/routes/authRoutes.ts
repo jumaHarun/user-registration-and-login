8123d3f2967819dba73d641fe1a11c3713e267a3
Juma Harun <mmbh4run@gmail.com>
2024-09-19 03:31:51 +0300

    Added the refresh route

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,9 +1,15 @@
 import express from "express";
-import { registerUser, loginUser } from "../controllers/authController.ts";
+import {
+  registerUser,
+  loginUser,
+  refresh,
+} from "../controllers/authController.ts";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+
+router.post("/refresh", refresh);
 
 export default router;
